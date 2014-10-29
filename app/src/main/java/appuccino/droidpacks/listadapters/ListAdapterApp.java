ea2bbce98d1abad28c258b6370f26863c9bc9465
Waylon Brown <waylonjbrown@gmail.com>
2014-10-29 00:12:45 -0500

    Fixed horizontallistview header/footer problem

--- app/src/main/java/appuccino/droidpacks/listadapters/ListAdapterApp.java
+++ app/src/main/java/appuccino/droidpacks/listadapters/ListAdapterApp.java
@@ -18,6 +18,11 @@ import appuccino.droidpacks.objects.App;
 
 public class ListAdapterApp extends ArrayAdapter<App> {
 
+    private static final int VIEW_TYPE_HEADER = 0;
+    private static final int VIEW_TYPE_FOOTER = 1;
+    private static final int VIEW_TYPE_DEFAULT = 2;
+    private static final int VIEW_TYPE_COUNT = 3;
+
     Context context;
     int layoutResourceId;
     List<App> appList;
@@ -30,24 +35,48 @@ public class ListAdapterApp extends ArrayAdapter<App> {
     }
 
     @Override
+    public int getViewTypeCount() {
+        // The total number of row types your adapter supports.
+        // This should NEVER change at runtime.
+        return VIEW_TYPE_COUNT;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // return a value from zero to (viewTypeCount - 1)
+        if (position == 0) {
+            return VIEW_TYPE_HEADER;
+        } else if (position == getCount() - 1) {
+            return VIEW_TYPE_FOOTER;
+        }
+        return VIEW_TYPE_DEFAULT;
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        int layoutID = 0;
+
+        switch(getItemViewType(position)) {
+            case VIEW_TYPE_HEADER:
+                layoutID = R.layout.list_column_appheaderitem;
+                break;
+            case VIEW_TYPE_FOOTER:
+                layoutID = R.layout.list_column_appfooteritem;
+                break;
+            case VIEW_TYPE_DEFAULT:
+                layoutID = layoutResourceId;
+
+        }
+
         View row = convertView;
         AppHolder holder = null;
 
         if(row == null)
         {
-
-
             holder = new AppHolder();
             LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            if(position == 0){
-                holder.rowLayoutID = R.layout.list_column_appheaderitem;
-            } else if(position == appList.size() - 1){
-                holder.rowLayoutID = R.layout.list_column_appfooteritem;
-            } else {
-                holder.rowLayoutID = layoutResourceId;
-            }
-            row = inflater.inflate(holder.rowLayoutID, parent, false);
+            row = inflater.inflate(layoutID, parent, false);
 
             holder.appIcon = (ImageView)row.findViewById(R.id.appIcon);
 
